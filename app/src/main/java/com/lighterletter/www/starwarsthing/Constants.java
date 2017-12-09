5197ba39fcbb171da8ea12605475da8217d6edb0
lighterletter <jgomez818@gmail.com>
2017-12-09 14:11:16 -0500

    added json

---
+++ app/src/main/java/com/lighterletter/www/starwarsthing/Constants.java
@@ -0,0 +1,41 @@
+package com.lighterletter.www.starwarsthing;
+
+/**
+ * https://swapi.co/api/people/?format=json&page=1
+ */
+
+public class Constants {
+
+    public final static String jsonSample = "{\n" +
+            "name: \"Luke Skywalker\",\n" +
+            "height: \"172\",\n" +
+            "mass: \"77\",\n" +
+            "hair_color: \"blond\",\n" +
+            "skin_color: \"fair\",\n" +
+            "eye_color: \"blue\",\n" +
+            "birth_year: \"19BBY\",\n" +
+            "gender: \"male\",\n" +
+            "homeworld: \"https://swapi.co/api/planets/1/\",\n" +
+            "films: [\n" +
+            "\"https://swapi.co/api/films/2/\",\n" +
+            "\"https://swapi.co/api/films/6/\",\n" +
+            "\"https://swapi.co/api/films/3/\",\n" +
+            "\"https://swapi.co/api/films/1/\",\n" +
+            "\"https://swapi.co/api/films/7/\"\n" +
+            "],\n" +
+            "species: [\n" +
+            "\"https://swapi.co/api/species/1/\"\n" +
+            "],\n" +
+            "vehicles: [\n" +
+            "\"https://swapi.co/api/vehicles/14/\",\n" +
+            "\"https://swapi.co/api/vehicles/30/\"\n" +
+            "],\n" +
+            "starships: [\n" +
+            "\"https://swapi.co/api/starships/12/\",\n" +
+            "\"https://swapi.co/api/starships/22/\"\n" +
+            "],\n" +
+            "created: \"2014-12-09T13:50:51.644000Z\",\n" +
+            "edited: \"2014-12-20T21:17:56.891000Z\",\n" +
+            "url: \"https://swapi.co/api/people/1/\"\n" +
+            "}";
+}
